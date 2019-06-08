88dc1be1b4acd1745c8e18871eed6bddb8808e4d
palanibsm <senthil2world@gmail.com>
2019-06-08 19:18:10 +1200

    ec2chg

--- aws-sen/main.tf
+++ aws-sen/main.tf
@@ -1,5 +1,11 @@
+# Configure the AWS Provider
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-east-2"
+}
+
 resource "aws_instance" "firstdemo" {
-  ami           = "ami-922914f7"
+  ami           = "ami-0d3ebe261ec589631"
   instance_type = "t2.micro"
 
   tags = {
