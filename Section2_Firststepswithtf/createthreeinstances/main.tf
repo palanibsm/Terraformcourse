d25894e4872094543e67a32666c2717858e1e738
palanibsm <senthil2world@gmail.com>
2019-06-08 20:20:01 +1200

    ec2

--- Section2_Firststepswithtf/createthreeinstances/main.tf
+++ Section2_Firststepswithtf/createthreeinstances/main.tf
@@ -1,15 +1,21 @@
 
+
+# Configure the AWS Provider
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-east-2"
+}
 resource "aws_instance" "firstdemo" {
 
   count     = 3
 
-  ami           = "ami-922914f7"
+  ami           = "ami-04768381bf606e2b3"
 
   instance_type = "t2.micro"
 
 
 
-  tags {
+  tags = {
 
     Name = "threedemoinstance-${count.index}"
 
