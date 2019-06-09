927102254e6cd9264a0dbcbf61242704108d8970
palanibsm <senthil2world@gmail.com>
2019-06-09 12:15:45 +1200

    t

--- Section3_Usingvariables/mapvariables_example1/main.tf
+++ Section3_Usingvariables/mapvariables_example1/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "firstdemo" {
 
 ami = "${var.amitype}"
 
-instance_type="${lookup(var.instance_type,var.env, 't2.nano')}"
+instance_type="${lookup(var.instance_type,var.env,t2.nano)}"
 
 tags = {
 
